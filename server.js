f0073aa01c64cff6a579497145dfb93913a8a621
New Dad <josh_panakkal3122@hotmail.com>
2020-08-09 15:58:42 -0400

    change api get route to read json file using file system

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const db = require("../note_taker/db/db.json");
+const db = require("./db/db.json");
+const fs = require("fs");
+const { json } = require("express");
 
 
 
@@ -18,8 +20,7 @@ app.use(express.json());
 
 // Routes
 // =============================================================
-
-// Basic route that sends the user first to the AJAX Page
+// HTML Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });   
@@ -28,11 +29,20 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
     });
 
+//API Routes
 app.get("/api/notes", function(req, res) {
   
-  res.sendFile(path.join(__dirname, 'db/db.json'));
+  fs.readFile("./db/db.json", "utf8", function(error, data){
+    if(error){
+      console.log(error);
+    }
+    else{
+      // console.log(JSON.parse(data));
+      return JSON.parse(data);
+    }
+  })
   
-    });
+});
 app.get('/api/notes/:id', function(req, res){
   var chosen = req.params.id;
   console.log(chosen);  
@@ -43,9 +53,21 @@ app.get('/api/notes/:id', function(req, res){
       return res.json(db[i]);      
     }
   }
-
   return res.send("ID not found");
 
+});
+
+// Create New Notes - POST request
+app.post("/api/notes", function(req, res){
+  var newNote = req.body;
+
+  newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newNote);
+
+  db.push(newNote);
+  
+  res.json(newNote);
 })
   
 
