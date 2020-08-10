84722bbc9c9baf46cb3217011f1cb7cd6eb93bce
New Dad <josh_panakkal3122@hotmail.com>
2020-08-10 18:15:02 -0400

    add app.use static and delete route

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
 const fs = require("fs");
-const { json } = require("express");
-
-
 
 // Sets up the Express App
 // =============================================================
@@ -16,18 +13,14 @@ const PORT = 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"))
 
 // Routes
 // =============================================================
-// HTML Routes
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
-  });   
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-    });
+  }); 
 
 //API Routes
 app.get("/api/notes", function(req, res) {
@@ -36,44 +29,63 @@ app.get("/api/notes", function(req, res) {
     if(error){
       console.log(error);
     }
-    else{
-      // console.log(JSON.parse(data));
-      return JSON.parse(data);
+    else{      
+      res.json(JSON.parse(data));
     }
-  })
-  
-});
-app.get('/api/notes/:id', function(req, res){
-  var chosen = req.params.id;
-  console.log(chosen);  
-  
- //creates a dynamic route and sends back json information
-  for(var i = 0; i < db.length; i++){
-    if(chosen === db[i].id){
-      return res.json(db[i]);      
-    }
-  }
-  return res.send("ID not found");
-
+  })  
 });
 
 // Create New Notes - POST request
 app.post("/api/notes", function(req, res){
   var newNote = req.body;
+  newNote.id = Date.now()
 
-  newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
+  fs.readFile("./db/db.json", "utf8", function(error, data){
+    if(error){
+      console.log(error);
+    }
+    else{
+      var notes = JSON.parse(data);
+      notes.push(newNote);
 
-  console.log(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes), function(err){
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("Success!");
+          res.json(newNote);
+        }
+      })    
+    }
+  })  
+});
 
-  db.push(newNote);
+app.delete('/api/notes/:id', function(req, res){
+  var chosen = req.params.id;
+  console.log(chosen);
+  var array = [];
   
-  res.json(newNote);
-})
-  
+ //creates a dynamic route and sends back json information
+  for(var i = 0; i < db.length; i++){
+    if(chosen !== db[i].id){
+      array.push(db[i]);
+    }
+  }
+  fs.writeFile("./db/db.json", JSON.stringify(array), function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Success!");
+      res.json(array);      
+    }
+  })  
+});
 
-  
-
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 
 // Starts the server to begin listening
 // =============================================================
